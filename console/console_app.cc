0f8d4c5c1b815f09e77ece5e666b3a73e5640415
ynnmuraii <84408793+ynnmuraii@users.noreply.github.com>
2023-12-25 16:45:11 +0400

    исправление работ операторов

--- console/console_app.cc
+++ console/console_app.cc
@@ -4,7 +4,22 @@ using namespace std;
 using namespace vectors;
 
 int main() {
-    Line<float> triangle = draw_broken_line_triangle<float>();
+    /*Line<float> triangle = draw_broken_line_triangle<float>();
+    cout << triangle*/;
+
+    Point<float> vertex1(0, 0);
+    Point<float> vertex2(7, 0);
+    Point<float> vertex3(0.1, 3);
+
+    Line<float> triangle(vertex1);
+    Line<float> triangle1;
+
+    cout << triangle;
+
+    triangle1 = triangle + vertex2;
+
+    triangle = vertex1 + triangle1 + vertex3;
+
     cout << triangle;
 
     return 0;
